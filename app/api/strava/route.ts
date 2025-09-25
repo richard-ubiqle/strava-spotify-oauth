aae803b7ccdb5b7e5253311ef25118bc569ff958
Mobolaji Richard <sinclairrichard79@gmail.com>
2025-09-25 16:02:08 +0100

    updated eslint

--- app/api/strava/route.ts
+++ app/api/strava/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import axios from "axios";
 import { PrismaClient } from "../../generated/prisma";
 
